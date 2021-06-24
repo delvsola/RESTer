dcb092024a2d814ff0454087d53a4bd9b0373dc4
Solan Delvenne <delvennesolan@gmail.com>
2021-06-24 10:41:25 +0200

    Fix issue with assigining values to point

--- utils/shapefile.py
+++ utils/shapefile.py
@@ -15,6 +15,6 @@ def get_bbox(pt: Point) -> [Tuple[Point], None]:
     if len(shape) == 0:
         return None
     bounds = shape.bounds
-    ul = Point(bounds.minx, bounds.maxy)
-    lr = Point(bounds.maxx, bounds.miny)
+    ul = Point(bounds.minx[0], bounds.maxy[0])
+    lr = Point(bounds.maxx[0], bounds.miny[0])
     return ul, lr
