2832efc2573ec0c463174c73d6c3f30d3f50f473
Solan Delvenne <delvennesolan@gmail.com>
2021-06-24 10:27:17 +0200

    Add function to get bounding box from Shapefile

---
+++ utils/shapefile.py
@@ -0,0 +1,18 @@
+import geopandas as gpd
+from shapely.geometry import Point, Polygon, MultiPolygon
+from typing import Tuple
+
+
+def get_bbox(pt: Point) -> [Tuple, None]:
+    """
+    Get the bounding box of a building on a Point
+    :param pt: Shapely point with the coordinates of the building
+    :return: A tuple with Upper-Left and Lower-Right coordinates of the box or
+    None if no building found
+    """
+    shape = gpd.read_file('./shapefile/belgium//Bpn_ReBu.shp',
+                          mask=pt)
+    if len(shape) == 0:
+        return None
+    bounds = shape.bounds
+    return bounds.minx, bounds.maxy, bounds.maxx, bounds.miny
